51f8d87e238010f03bc4a2f4feffff8b9f959d62
Jaime M Trillos <jaimemanueltrillos@gmail.com>
2023-05-29 15:41:11 +0200

    starting with multi page routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductPage from "./pages/Product";
+import RootLayout from "./pages/Root";
+
+/*const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/products" element={<ProductPage />} />
+  </Route>
+);
+
+const router = createBrowserRouter(routeDefinitions);*/
+
+/*const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductPage /> },
+]);*/
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
